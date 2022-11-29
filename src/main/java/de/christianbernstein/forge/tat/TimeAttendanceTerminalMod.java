b69606b4f8a40143864a1de08c01407d72f804de
Christian Bernstein <59013801+christian-bernstein@users.noreply.github.com>
2022-11-29 18:02:26 +0100

    unnamed

--- src/main/java/de/christianbernstein/forge/tat/TimeAttendanceTerminalMod.java
+++ src/main/java/de/christianbernstein/forge/tat/TimeAttendanceTerminalMod.java
@@ -23,8 +23,7 @@ import java.util.stream.Collectors;
 // The value here should match an entry in the META-INF/mods.toml file
 // @Mod("timeattendanceterminal")
 @Mod("examplemod")
-public class TimeAttendanceTerminalMod
-{
+public class TimeAttendanceTerminalMod {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
